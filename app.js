d99b7edb75337203a94cb5b4e90b3950d6f1ddfa
cgarbe7059 <102381559+cgarbe7059@users.noreply.github.com>
2022-07-12 21:50:04 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+msg = "Title";
+msg = " ...Just a scrolling title example" + msg;position = 0;
+function scrolltitle() {
+document.title = msg.substring(position, msg.length) + msg.substring(0, position); position++;
+if (position > msg.length) position = 0
+window.setTimeout("scrolltitle()",170);
+}
+scrolltitle();
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
